68288ee40a23ab6c7ccd3f517f54e440dad6e545
ImTaliesin <brennanjdouglas@gmail.com>
2023-07-31 20:41:03 -0700

    pre-component

---
+++ src/components/descriptions.jsx
@@ -0,0 +1,9 @@
+export default function Descriptions(props) {
+    return (
+        <div className="descriptions">
+        <h1>{props.title}</h1>
+        <p>{props.description}</p>
+        </div>
+    );
+}
+    
---
+++ src/components/icons.jsx
@@ -0,0 +1,7 @@
+export default function Icons(props) {
+    return (
+        <div className="icons">
+        <p>{props.image}</p>
+        </div>
+    );
+}
